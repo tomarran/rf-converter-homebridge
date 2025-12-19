bfbb0a10cbc3f5226fea3d136d55e5e0ccba3d2e
tomarran <5328772+tomarran@users.noreply.github.com>
2025-12-19 14:19:02 +0200

    Create settings.ts

---
+++ src/settings.ts
@@ -0,0 +1,9 @@
+/**
+ * This must match the name of your plugin as defined the package.json
+ */
+export const PLUGIN_NAME = 'homebridge-rf-converter';
+
+/**
+ * This must match the settings.json 'pluginAlias'
+ */
+export const PLATFORM_NAME = 'RFConverterPlatform';
